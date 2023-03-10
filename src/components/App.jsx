1f3efe07f6d77da270a34a8227a0799cd73f3d55
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-03-11 00:08:40 +0200

    webpack was created with React

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,57 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import ContactList from './contactList/ContactList';
+import Filter from './filter/Filter';
+import ContactForm from './form/ContactForm';
+import uniqid from 'uniqid';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  handleSubmit = ({ name, number }) => {
+    const ID = uniqid.process();
+    const string = this.state.contacts.filter(el => el.name === name);
+    string.length !== 0
+      ? this.hendleCoincidence(name)
+      : this.setState(({ contacts }) => ({
+          contacts: [...contacts, { id: ID, name, number }],
+        }));
+  };
+
+  hendleCoincidence(name) {
+    alert(`${name} is already in contacts`);
+  }
+
+  hendleSearch = event => {
+    const value = event.currentTarget.value.toLowerCase().trim();
+    this.setState({ filter: value });
+  };
+
+  hendeleClickDelete = id => {
+    this.setState(({ contacts }) => ({
+      contacts: contacts.filter(el => el.id !== id),
+    }));
+  };
+
+  render() {
+    const { filter, contacts } = this.state;
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm submit={this.handleSubmit} contacts={this.contacts} />
+
+        <h2>Contacts</h2>
+        <Filter filter={this.hendleSearch} />
+        <ContactList
+          contacts={contacts}
+          filter={filter}
+          remove={this.hendeleClickDelete}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/contactList/ContactList.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+
+const ContactList = ({ contacts, filter, remove }) => {
+  return (
+    <ul>
+      {contacts.map(({ id, name, number }) => {
+        const lowerCase = name.toLowerCase();
+        if (lowerCase.includes(filter)) {
+          return (
+            <li key={id}>
+              <p>{name}</p>
+              <span>{number}</span>
+              <button onClick={() => remove(id)}>Delete</button>
+            </li>
+          );
+        }
+      })}
+    </ul>
+  );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  filter: PropTypes.string.isRequired,
+};
+export default ContactList;
